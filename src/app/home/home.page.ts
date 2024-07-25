1022e82ecc5a410eb539f70ab7b3dd4537a92051
VICJR31 <alejandroromo31st@gmail.com>
2024-07-25 15:35:52 -0700

    Updated scrollToText feature and overall UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  scrollToElement(targetId: string) {
+    console.log('Scrolling to:', targetId);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
